2131181877a3ad7feeada2ffc90dfc545ab69a0c
Irfan858 <irfankurniawan858@gmail.com>
2022-08-03 15:08:43 +0700

    Menyelesaikan FItur Validasi Data

--- input-data-bayi-proses.php
+++ input-data-bayi-proses.php
@@ -8,19 +8,36 @@
     $tb_lahir = $_POST['tb_lahir'];
     $nama_ortu = $_POST['nama_ortu'];
 
-    $query = mysqli_query($koneksi, "INSERT INTO data_bayi (nama_bayi, jenis_kelamin, tgl_lahir, bb_lahir, tb_lahir, nama_ortu ) VALUES (
-        '$nama_bayi','$jenis_kelamin','$tanggal_lahir','$bb_lahir','$tb_lahir','$nama_ortu')");
+    // Validasi Sebelum Input Data Ke Database
+        // Validasi Umur
+        $tahun_sekarang = date('Y');
+        $tahun_lahir = substr($tanggal_lahir, 0,4);
+        $umur = $tahun_sekarang - $tahun_lahir;
 
-    // echo("INSERT INTO data_bayi (nama_bayi, jenis_kelamin, tanggal_lahir, bb_lahir, tb_lahir, nama_ortu ) VALUES ('$nama_bayi','$jenis_kelamin','$tanggal_lahir','$bb_lahir','$tb_lahir','$nama_ortu')");
-    
-    if($query)
-    {
-        echo "Data Berhasil Disimpan";
+        // Validasi Data Duplikat Berdasarkan Nama Bayi
+        $cek_data_bayi = mysqli_num_rows(mysqli_query($koneksi,"SELECT * FROM data_bayi WHERE nama_bayi = '$nama_bayi' AND tgl_lahir = '$tanggal_lahir'"));
+
+    if($cek_data_bayi > 0) {
+        echo "Data Sudah Ada";
         echo "<meta http-equiv='refresh' content='2; url=data-bayi.php'>";
     }
-    else
-    {
-        echo "Data Gagal Tersimpan";
-        // mysqli_error($query);
+    else if($umur < 3) {
+        $query = mysqli_query($koneksi, "INSERT INTO data_bayi (nama_bayi, jenis_kelamin, tgl_lahir, bb_lahir, tb_lahir, nama_ortu ) VALUES (
+            '$nama_bayi','$jenis_kelamin','$tanggal_lahir','$bb_lahir','$tb_lahir','$nama_ortu')");
+        if($query)
+        {
+            echo "Data Berhasil Ditambahkan";
+            echo "<meta http-equiv='refresh' content='2; url=data-bayi.php'>";
+        }
+        else
+        {
+            echo "Data Gagal Ditambahkan";
+            mysqli_error($koneksi);
+        }
     }
+    else {
+        echo "Umur Bayi Sudah Tidak Valid";
+        echo "<meta http-equiv='refresh' content='2; url=data-bayi.php'>";
+    }
+   
 ?>
--- input-data-ibu-hamil-proses.php
+++ input-data-ibu-hamil-proses.php
@@ -7,19 +7,25 @@
     $nama_suami = $_POST['nama_suami'];
     $no_hp = $_POST['no_hp'];
 
-    $query = mysqli_query($koneksi, "INSERT INTO data_ibu_hamil (nama, tgl_lahir, alamat, nama_suami, no_hp ) VALUES (
-        '$nama','$tgl_lahir','$alamat','$nama_suami','$no_hp')");
-
-    // echo("INSERT INTO data_ibu_hamil (nama, tgl_lahir, alamat, nama_suami, no_hp ) VALUES ('$nama','$tgl_lahir','$alamat','$nama-suami','$no_hp')");
+    // Validasi Data Ganda Berdasarkan nama dan nama_suami
+    $cek_data_ibu_hamil = mysqli_num_rows(mysqli_query($koneksi,"SELECT * FROM data_ibu_hamil WHERE nama = '$nama' AND nama_suami = '$nama_suami'"));
     
-    if($query)
-    {
-        echo "Data Berhasil Disimpan";
+    if($cek_data_ibu_hamil > 0) {
+        echo "Data Sudah Ada";
         echo "<meta http-equiv='refresh' content='2; url=data-ibu-hamil.php'>";
     }
-    else
-    {
-        echo "Data Gagal Tersimpan";
-        // mysqli_error($query);
+    else {
+        $query = mysqli_query($koneksi, "INSERT INTO data_ibu_hamil (nama, tgl_lahir, alamat, nama_suami, no_hp) VALUES (
+            '$nama','$tgl_lahir','$alamat','$nama_suami','$no_hp')");
+        if($query)
+        {
+            echo "Data Berhasil Ditambahkan";
+            echo "<meta http-equiv='refresh' content='2; url=data-ibu-hamil.php'>";
+        }
+        else
+        {
+            echo "Data Gagal Tersimpan";
+            mysqli_error($koneksi);
+        }
     }
 ?>
